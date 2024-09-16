8b9a996312836a1f8e8b225df05b7d9dc7441364
Emily <e.spiers13@gmail.com>
2024-09-16 10:28:51 +0100

    allow cross origin resource sharing

--- app/server.js
+++ app/server.js
@@ -17,6 +17,7 @@ const {
   handleServerErrors,
 } = require("./errors");
 const apiRouter = require("./routers/api-routers");
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
